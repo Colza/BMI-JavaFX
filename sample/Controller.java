43e19597c23e5847b94a044e42616fc746c76d0a
colza <Julian@julianholden.com>
2018-12-07 21:25:28 +0000

    Added weight message and tidied code.

--- sample/Controller.java
+++ sample/Controller.java
@@ -12,13 +12,23 @@ public class Controller {
     public TextField height;
     public Button calculate;
     public Label result;
+    public Label rating;
 
 
     public void clickAction(ActionEvent actionEvent) {
         System.out.println(weight.getText());
-       double w = Double.parseDouble(weight.getText());
-       double h = Double.parseDouble(height.getText());
-       double bmi = w / (h * h);
-       result.setText("Your BMI is " + bmi);
+        double w = Double.parseDouble(weight.getText());
+        double h = Double.parseDouble(height.getText());
+        double bmi = w / (h * h);
+        String rat;
+        if (bmi < 20)  rat = "Underweight";
+         else
+            if (bmi < 25) rat = "Normal";
+         else
+            if (bmi < 30) rat = "Overweight";
+        else rat = "obese";
+
+    result.setText("Your BMI is " + String.format("%.2f", bmi));
+    rating.setText(rat);
     }
 }
